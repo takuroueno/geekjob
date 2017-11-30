b8e23248021cefdbda527542af1b187c2f9bc4ac
上野拓朗 <>
2017-11-30 14:29:48 +0900

    no message

---
+++ CampChallenge_java/013.クラス/クラスの継承.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author uenotakuro
+ */
+public class person extends Human {
+
+    String msg = "講師の" + this.name + "です。講義を始めます。";
+
+}
+
+class Test2 {
+
+    public static void main(String[] args) {
+
+        Human hito = new Human();
+        hito.setHuman("添田亮司", 34);
+
+        hito.setHuman("", 0);
+        hito.printHuman();
+
+    }
+}
